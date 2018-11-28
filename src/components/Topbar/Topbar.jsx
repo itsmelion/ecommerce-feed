2ff5a1f7cd27d89b1dd416e1e2290c435d0c9458
Chris Lion <christhopherleao@icloud.com>
2018-11-28 21:03:56 -0200

    added a link to go back to Home(list)

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Topbar.scss';
+import { ReactComponent as Logo } from './logo.svg';
 
 const Topbar = () => (
-  <nav id="Topbar">
-    Navigation
+  <nav row="nowrap" align="start center" id="Topbar">
+    <Link className="menu-logo" to="/list">
+      <Logo />
+    </Link>
+
+    {/* Search is here only for aestethics */}
+    <input
+      flex=""
+      className="hide-mobile mh2"
+      placeholder="Search"
+      type="search"
+      name="search"
+      id="search"
+    />
   </nav>
 );
 
