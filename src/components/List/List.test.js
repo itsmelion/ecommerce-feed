eb0abada504d438804fa40548a4269927a7258a9
Chris Lion <christhopherleao@icloud.com>
2018-12-01 18:14:34 -0200

    Comment LIST test

--- src/components/List/List.test.js
+++ src/components/List/List.test.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { render } from 'enzyme';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { MemoryRouter as Router } from 'react-router-dom';
 import data from 'ads.json';
 import List from './List';
 
 let wrapper;
+
+/*
+  The following IIFE is returns the amount of ads on data provided
+  to later check if we rendered them all.
+*/
+const dataAmountItems = (() => {
+  let sum = 0;
+  data.forEach(({ ads }) => {
+    sum += ads.length;
+  });
+  return sum;
+})();
 
 beforeEach(() => {
   wrapper = render(<Router><List /></Router>);
@@ -12,13 +24,5 @@ beforeEach(() => {
 
 // More or less like an integration test
 it('Amount of Items rendered match data provided', () => {
-  const dataAmountItems = (() => {
-    let sum = 0;
-    data.forEach(({ ads }) => {
-      sum += ads.length;
-    });
-    return sum;
-  })();
-
   expect(wrapper.find('li').length).toEqual(dataAmountItems);
 });
